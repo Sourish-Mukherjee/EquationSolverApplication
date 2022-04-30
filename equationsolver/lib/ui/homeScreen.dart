b2f9b29488f8cb0e08f4ee9d9d46881f1381c1ff
Sourish Mukherjee <56296940+Sourish-Mukherjee@users.noreply.github.com>
2022-04-30 22:03:52 +0530

    Added Camera

--- equationsolver/lib/ui/homeScreen.dart
+++ equationsolver/lib/ui/homeScreen.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:equationsolver/ui/components/elevatedbutton.dart';
 import 'package:equationsolver/ui/finalScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,6 +14,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final ImagePicker _picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +27,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 colors: const [Colors.black, Color.fromARGB(255, 61, 61, 64)])),
         child: Center(
           child: MyElevatedButton(
-            onPressed: () {
-              Navigator.push(
+            onPressed: () async {
+              final XFile? pickedFile =
+                  await _picker.pickImage(source: ImageSource.camera);
+              print(pickedFile);
+              Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(
                     builder: (context) => FinalScreen(),
